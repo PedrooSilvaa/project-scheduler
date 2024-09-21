ee3f07b791c253a0137526a44220419bcfa35a1b
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-09-21 13:54:40 -0300

    feat: create project

---
+++ src/main/java/tech/silva/scheduler/ProjectSchedulerApplication.java
@@ -0,0 +1,13 @@
+package tech.silva.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/silva/scheduler/ProjectSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.silva.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
